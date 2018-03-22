fb60c1b92c68c17708dca4213eba23f4fc709bd3
Oleg Khokhlov <olegish@gmail.com>
2018-03-23 01:59:31 +0300

    encoder float point support
    mlz encoder

--- libavcodec/mlz.c
+++ libavcodec/mlz.c
@@ -198,3 +198,392 @@ int ff_mlz_decompression(MLZ* mlz, GetBitContext* gb, int size, unsigned char *b
     }
     return output_chars;
 }
+
+
+
+void mlz_create(MLZ* mlz)
+{
+    mlz_alloc_dict(mlz);
+    mlz_init_dict(mlz);
+    mlz_flush_dict(mlz);
+}
+
+void mlz_delete(MLZ* mlz)
+{
+    mlz_free_dict(mlz);
+}
+
+void mlz_alloc_dict(MLZ* mlz)
+{
+    int i;
+    //pDict       = new DICT [ TABLE_SIZE ];
+    //ppHashTable = new int * [ TABLE_SIZE ];
+    //for ( i = 0; i < TABLE_SIZE; i++ ) ppHashTable[i] = new int [ WORD_SIZE ];
+    mlz->p_dict = (MLZDict*)malloc(sizeof(MLZDict) * TABLE_SIZE);
+    mlz->pp_hash_table = (int**)malloc(sizeof(int*) * TABLE_SIZE);
+    for(i = 0; i < TABLE_SIZE; i++)
+        mlz->pp_hash_table[i] = (int*)malloc(sizeof(int) * WORD_SIZE);
+
+
+    //for encoder
+    //b_pDict       = new DICT [ TABLE_SIZE ];
+    //b_ppHashTable = new int * [ TABLE_SIZE ];
+    //for ( i = 0; i < TABLE_SIZE; i++ ) b_ppHashTable[i] = new int [ WORD_SIZE ];
+    mlz->bp_dict = (MLZDict*)malloc(sizeof(MLZDict) * TABLE_SIZE);
+    mlz->bpp_hash_table = (int**)malloc(sizeof(int*) * TABLE_SIZE);
+    for(i = 0; i < TABLE_SIZE; i++)
+        mlz->bpp_hash_table[i] = (int*)malloc(sizeof(int) * WORD_SIZE);
+}
+
+void mlz_init_dict(MLZ* mlz)
+{
+    mlz->m_flush_code = FLUSH_CODE;
+    mlz->m_current_dict_index_max = DIC_INDEX_INIT;
+    mlz->m_dict_code_bit = CODE_BIT_INIT;
+    mlz->m_bump_code = (DIC_INDEX_INIT) - 1;
+    mlz->m_next_code = FIRST_CODE;
+    mlz->m_freeze_flag = 0;
+}
+
+
+
+
+void mlz_flush_dict(MLZ* mlz)
+{
+    int i, j;
+    for ( i = 0; i < TABLE_SIZE; i++ ) {
+        mlz->p_dict[i].string_code = CODE_UNSET;
+        mlz->p_dict[i].parent_code = CODE_UNSET;
+        mlz->p_dict[i].match_len = 0;
+        for ( j=0; j < WORD_SIZE; j++ )
+            mlz->pp_hash_table[i][j] = CODE_UNSET;
+    }
+    //// read first part
+    // initial DicCodes
+    // $0 - 255 xxxx
+    // $256 FLUSH_CODE
+    // $257 FREEZE_CODE
+    // $258 - $(max-2) code
+    // $(max-1) BUMP_CODE
+    // $(max-1) BumpCode  1st BumpCode = 511
+    // add first entry to dictionary as [$258]
+    mlz->m_current_dict_index_max = DIC_INDEX_INIT;
+    mlz->m_dict_code_bit = CODE_BIT_INIT;  // DicCodeBitInit;
+    mlz->m_bump_code = mlz->m_current_dict_index_max - 1;
+    mlz->m_next_code = FIRST_CODE;
+    mlz->m_freeze_flag = 0;
+}
+
+void mlz_free_dict(MLZ* mlz)
+{
+    int i;
+    if ( mlz->p_dict != NULL ) {
+        free(mlz->p_dict);
+        mlz->p_dict = NULL;
+    }
+    if ( mlz->pp_hash_table != NULL ) {
+        for ( i = 0; i < TABLE_SIZE; i++ ) free(mlz->pp_hash_table[i]);
+        free(mlz->pp_hash_table);
+        mlz->pp_hash_table = NULL;
+    }
+
+    if ( mlz->bp_dict != NULL ) {
+        free(mlz->bp_dict);
+        mlz->bp_dict = NULL;
+    }
+    if ( mlz->bpp_hash_table != NULL ) {
+        for ( i = 0; i < TABLE_SIZE; i++ ) free(mlz->bpp_hash_table[i]);
+        free(mlz->bpp_hash_table);
+        mlz->bpp_hash_table = NULL;    }
+}
+
+
+void mlz_backup_dict(MLZ* mlz)
+{
+    int i, j;
+    for ( i = 0; i < TABLE_SIZE; i++ ) {
+        mlz->bp_dict[i].string_code = mlz->p_dict[i].string_code;
+        mlz->bp_dict[i].char_code   = mlz->p_dict[i].char_code;
+        mlz->bp_dict[i].match_len   = mlz->p_dict[i].match_len;
+        mlz->bp_dict[i].parent_code = mlz->p_dict[i].parent_code;
+        for ( j = 0; j < WORD_SIZE; j++ ) {
+            mlz->bpp_hash_table[i][j] = mlz->pp_hash_table[i][j];
+        }
+    }
+    mlz->b_current_dict_index_max = mlz->m_current_dict_index_max;
+    mlz->b_dict_code_bit = mlz->m_dict_code_bit;
+    mlz->b_bump_code = mlz->m_bump_code;
+    mlz->b_next_code = mlz->m_next_code;
+    mlz->b_freeze_flag = mlz->m_freeze_flag;
+}
+
+
+void mlz_resume_dict(MLZ* mlz)
+{
+    int i, j;
+    for ( i = 0; i < TABLE_SIZE; i++ ) {
+        mlz->p_dict[i].string_code = mlz->bp_dict[i].string_code;
+        mlz->p_dict[i].char_code   = mlz->bp_dict[i].char_code;
+        mlz->p_dict[i].match_len   = mlz->bp_dict[i].match_len;
+        mlz->p_dict[i].parent_code = mlz->bp_dict[i].parent_code;
+        for ( j = 0; j < WORD_SIZE; j++ ) {
+            mlz->pp_hash_table[i][j] = mlz->bpp_hash_table[i][j];
+        }
+    }
+    mlz->m_current_dict_index_max = mlz->b_current_dict_index_max;
+    mlz->m_dict_code_bit         = mlz->b_dict_code_bit;
+    mlz->m_bump_code           = mlz->b_bump_code;
+    mlz->m_next_code           = mlz->b_next_code;
+    mlz->m_freeze_flag         = mlz->b_freeze_flag;
+}
+
+unsigned long mlz_encode(     //ret: encoded size (bits)
+ MLZ* mlz,
+ unsigned char *p_input_buff,     //in: input string buffer inputBuff[sizeofInBuff]
+ unsigned char *p_input_mask,     //in: input mask buffer inputMask[sizeofInBuff]
+ unsigned long sizeof_input_buff, //in: buffer size
+ unsigned char *p_encode_buff,    //out: coded data
+ unsigned long sizeof_encode_buff //in: buffer size
+)
+{
+    int             last_match_len, match_len;
+    int             string_code, last_string_code, parent_code, char_code;
+    unsigned long   position, output_bits;
+
+    //set buffer information
+    mlz->m_sizeof_input_buff   = sizeof_input_buff;
+    mlz->m_sizeof_encode_buff  = sizeof_encode_buff;
+    mlz->mp_input_buff        = p_input_buff;
+    mlz->mp_input_mask        = p_input_mask;
+    mlz->mp_encode_buff       = p_encode_buff;
+    mlz->mp_out_position      = 0;
+
+    position = 0;
+    output_bits = 0;
+    last_string_code = -1;
+
+    while ( position < sizeof_input_buff ) {
+        // search dictionary
+        match_len = mlz_search_dict( mlz, last_string_code, &string_code, position ); // in: position / out: stringCode
+
+        //Output Longest match code
+        output_bits += mlz_output_code( mlz, string_code );
+        if ( position + match_len >= sizeof_input_buff ) {
+            position += match_len;
+            break;
+        } else {
+            if (( mlz->m_next_code + 1 >= (int )mlz->m_bump_code ) && ( mlz->m_current_dict_index_max >= DIC_INDEX_MAX )) {
+                output_bits += mlz_output_code( mlz, FLUSH_CODE );
+                mlz_flush_dict(mlz);
+                position += match_len;
+                last_string_code = -1;
+            } else {
+                if ( mlz->m_next_code + 1 >= (int )mlz->m_bump_code ) {
+                    output_bits += mlz_output_code( mlz, mlz->m_bump_code );
+                    mlz->m_current_dict_index_max *= 2;
+                    mlz->m_bump_code = mlz->m_current_dict_index_max - 1;
+                    mlz->m_dict_code_bit++;
+                }
+
+                char_code = mlz_get_root_index( mlz, position + match_len );
+                mlz_set_new_entry_to_dict_with_hash( mlz, mlz->m_next_code, string_code, char_code, match_len + 1 );
+
+                parent_code = mlz->m_next_code;
+                mlz->m_next_code++;
+    
+                position += match_len;
+                last_string_code = char_code;
+                last_match_len = match_len;
+                // check index code
+            }
+        }
+    }
+
+    return output_bits;
+
+
+
+
+
+}
+
+int mlz_output_code(MLZ* mlz, int string_code)
+{
+    int  i;
+    for ( i = 0; i < mlz->m_dict_code_bit; i++ ) {
+        if ( mlz->mp_out_position >= mlz->m_sizeof_encode_buff ) return i;
+        mlz->mp_encode_buff[mlz->mp_out_position++] = (unsigned char )( (string_code >> (mlz->m_dict_code_bit - i - 1)) & 0x01 );
+    }
+    return mlz->m_dict_code_bit;
+}
+
+
+int mlz_get_vacant_hash_index(MLZ* mlz, int parent_code, int char_code, int mask_size)
+{
+    int hash_index;
+    int offset;
+
+    hash_index = 0;
+    hash_index = ( char_code << ( CODE_BIT_MAX - WORD_SIZE ) ) ^ parent_code;  // here, charCode == charCode & mask
+    if ( hash_index == 0 )
+        offset = 1;
+    else
+        offset = TABLE_SIZE - hash_index;
+    while ( mlz->pp_hash_table[ hash_index ][ mask_size % WORD_SIZE ] != CODE_UNSET )
+    {
+        hash_index -= offset;
+        if ( hash_index < 0 )
+            hash_index += TABLE_SIZE;
+    }
+    return hash_index;
+}
+
+void mlz_set_new_entry_to_dict_with_hash(MLZ* mlz, int string_code, int parent_code, int char_code, int match_len)
+{
+    unsigned int mask;
+    int hash_index;
+    int i;
+    
+    // add stringCode to pDict
+    mlz->p_dict[ string_code ].string_code = string_code;
+    mlz->p_dict[ string_code ].parent_code = parent_code;
+    mlz->p_dict[ string_code ].char_code   = char_code;
+    mlz->p_dict[ string_code ].match_len   = match_len;
+
+    hash_index = mlz_get_vacant_hash_index( mlz, parent_code, char_code, 0 );
+    
+    mlz->pp_hash_table[hash_index][0] = string_code;
+
+    for ( i = 1; i < WORD_SIZE; i++ ) {
+        mask = ( 0x01 << i ) - 1;
+        mask <<= ( WORD_SIZE - i );
+        hash_index = mlz_get_vacant_hash_index( mlz, parent_code, (char_code & mask), i );
+
+        mlz->pp_hash_table[hash_index][i] = string_code;
+    }
+}
+
+
+int mlz_get_hash_index(
+  MLZ* mlz,
+  int parent_code,       // in: parent index code of the dict.
+  int char_code,     // in: charCode = charCode & mask
+  int  mask_size,       // in: mask bit width
+  int *p_candidates, // out: list of stringCodes
+  int  num_index_max      // in: maxnum of candidates
+)
+{
+    int mask;
+    int  num_candidates, hash_index, offset;
+    int dflag; 
+
+    mask = ( 0x01 << mask_size ) - 0x01;
+    mask <<= ( WORD_SIZE - mask_size );
+
+    num_candidates = 0;
+    hash_index = 0;
+    hash_index = ( ( char_code & mask ) << ( CODE_BIT_MAX - WORD_SIZE ) ) ^ parent_code;  // here, charCode == charCode & mask
+    if ( hash_index == 0 )
+        offset = 1;
+    else
+        offset = TABLE_SIZE - hash_index;
+
+    while ( mlz->pp_hash_table[ hash_index ][ mask_size % WORD_SIZE ] != CODE_UNSET )
+    {
+        dflag = 1;
+        if ( mlz->p_dict[ mlz->pp_hash_table[ hash_index ][ mask_size % WORD_SIZE ] ].parent_code != parent_code ) {
+            dflag = 0;
+        }
+        if ( char_code != ( mlz->p_dict[ mlz->pp_hash_table[ hash_index ][ mask_size % WORD_SIZE ] ].char_code & mask ) ) {    // needs to be compared with mask????
+            dflag = 0;
+        }
+        if ( dflag == 1 ) {
+            p_candidates[ num_candidates++ ] = mlz->pp_hash_table[ hash_index ][ mask_size % WORD_SIZE ]; //stringCode
+            //return num_candidates;
+            if ( num_candidates >= num_index_max ) {
+                return num_candidates;
+            }
+        }
+        hash_index -= offset;
+        if(hash_index < 0)
+            hash_index += TABLE_SIZE;
+    }
+    return num_candidates;
+
+
+
+
+
+}
+
+int mlz_get_root_index(MLZ* mlz, unsigned long position)
+{
+    int char_code;
+    if ( position >= mlz->m_sizeof_input_buff ) return -1;
+    char_code  = mlz->mp_input_buff[position];
+    return char_code;
+}
+
+int mlz_search_dict(       // return: length of matched string indicated by the index
+  MLZ* mlz,
+  int last_char_code,     // stringCode which is based on the search
+  int *string_code,          // out: index of the dict.
+  unsigned long position    // in:  position of m_pInputBuff
+)
+{
+    int match_len, ret_match_len;
+    int last_string_code, ret_string_code, char_code;
+    unsigned char mask_size;
+    int hash_candidates[MAX_SEARCH];
+    int  num_candidates;
+    int  i;
+
+    //********************
+    //** FIND ROOT NODE **
+    //********************
+    // find first entry of first char
+    if( position >= mlz->m_sizeof_input_buff ) {
+        *string_code = -1;
+        return 0;
+    }
+    if ( last_char_code < 0 ) {
+        last_string_code = mlz_get_root_index( mlz, position );
+        match_len = 1;
+    } else if ( last_char_code < FIRST_CODE ) {
+        last_string_code = last_char_code;
+        match_len = 1;
+    } else {
+        last_string_code = last_char_code;
+        match_len = mlz->p_dict[ last_string_code ].match_len;
+    }
+
+    //*********************
+    //** FIND CHILD NODE **
+    //*********************
+    // find longer entry
+    *string_code  = last_string_code;
+    if ( position + 1 < mlz->m_sizeof_input_buff ) {
+        char_code  = mlz->mp_input_buff[position + 1];
+        mask_size = mlz->mp_input_mask[position + 1];
+        num_candidates = mlz_get_hash_index( mlz, last_string_code, char_code, mask_size, hash_candidates, MAX_SEARCH );
+        if ( num_candidates == 0 ) {
+            return match_len; 
+        } else {
+            if ( position + 2 < mlz->m_sizeof_input_buff ) {
+                for ( i = 0; i < num_candidates; i++ ) {
+                    ret_match_len = mlz_search_dict( mlz, hash_candidates[i], &ret_string_code, position + 1 );
+                    if ( ret_match_len > match_len ) {
+                        match_len    = ret_match_len;
+                        *string_code = ret_string_code;
+                    }
+                }
+            }
+        }
+    }
+    return match_len;
+
+
+
+}
+
+
--- libavcodec/mlz.h
+++ libavcodec/mlz.h
@@ -25,6 +25,7 @@
 
 #define CODE_UNSET          -1
 #define CODE_BIT_INIT       9
+#define CODE_BIT_MAX        15
 #define DIC_INDEX_INIT      512     // 2^9
 #define DIC_INDEX_MAX       32768   // 2^15
 #define FLUSH_CODE          256
@@ -32,7 +33,8 @@
 #define FIRST_CODE          258
 #define MAX_CODE            32767
 #define TABLE_SIZE          35023   // TABLE_SIZE must be a prime number
-
+#define WORD_SIZE           8
+#define MAX_SEARCH          4
 /** Dictionary structure for mlz decompression
  */
 typedef struct MLZDict {
@@ -53,7 +55,110 @@ typedef struct MLZ {
     int freeze_flag;
     MLZDict* dict;
     void* context;
+
+    //////// fields for encoder/////////////////
+    MLZDict *p_dict;
+    int **pp_hash_table;
+    
+    // buffer information
+    //mp - pointers to main fields
+
+
+    unsigned char   *mp_input_buff;
+    unsigned char   *mp_input_mask;
+    unsigned long    m_sizeof_input_buff;
+    unsigned char   *mp_encode_buff;
+    unsigned long    m_sizeof_encode_buff;
+    unsigned long    mp_out_position;
+    
+    int              m_dict_code_bit;
+    int              m_current_dict_index_max;
+    unsigned int     m_bump_code;
+    unsigned int     m_flush_code;
+    int              m_next_code;
+    int              m_freeze_flag;
+
+    // dictionary backup area for the encoder
+    MLZDict *        bp_dict;
+    int **           bpp_hash_table;
+    int              b_dict_code_bit;
+    int              b_current_dict_index_max;
+    unsigned int     b_bump_code;
+    unsigned int     b_flush_code;
+    int              b_next_code;
+    int              b_freeze_flag;
+
+
+
+
+
 } MLZ;
+
+void mlz_create(MLZ* mlz);//+
+
+void mlz_delete(MLZ* mlz);//+
+
+void mlz_alloc_dict(MLZ* mlz);//+
+
+void mlz_free_dict(MLZ* mlz);//+
+
+void mlz_backup_dict(MLZ* mlz);//+
+
+void mlz_resume_dict(MLZ* mlz);//+
+
+void mlz_init_dict(MLZ* mlz);//+
+
+void mlz_flush_dict(MLZ* mlz);//+
+
+unsigned long mlz_encode(     //ret: encoded size (bits)
+ MLZ* mlz,
+ unsigned char *p_input_buff,     //in: input string buffer inputBuff[sizeofInBuff]
+ unsigned char *p_input_mask,     //in: input mask buffer inputMask[sizeofInBuff]
+ unsigned long sizeof_input_buff, //in: buffer size
+ unsigned char *p_encode_buff,    //out: coded data
+ unsigned long sizeof_encode_buff //in: buffer size
+);
+
+int mlz_output_code(MLZ* mlz, int string_code);//+
+
+int mlz_get_vacant_hash_index(//+
+    MLZ* mlz,
+    int parent_code,
+    int char_code,
+    int mask_size
+);
+
+void mlz_set_new_entry_to_dict_with_hash(//+
+  MLZ* mlz,
+  int string_code,
+  int parent_code,
+  int char_code,
+  int match_len
+);
+
+int mlz_get_hash_index(//+
+  MLZ* mlz,
+  int parent_code,       // in: parent index code of the dict.
+  int char_code,     // in: charCode = charCode & mask
+  int  mask_size,       // in: mask bit width
+  int *p_candidates, // out: list of stringCodes
+  int  num_index_max      // in: maxnum of candidates
+);
+
+int mlz_get_root_index(MLZ* mlz, unsigned long position);//+
+
+int mlz_search_dict(  //+     // return: length of matched string indicated by the index
+  MLZ* mlz,
+  int last_char_code,     // stringCode which is based on the search
+  int *string_code,          // out: index of the dict.
+  unsigned long position    // in:  position of m_pInputBuff
+);
+
+
+
+
+
+
 
 /** Initialize the dictionary
  */
